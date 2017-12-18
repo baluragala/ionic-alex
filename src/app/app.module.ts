c7c2820d9bc3097e277420cc4f93376fc14618ba
Balakrishna Ragala <baluragala@gmail.com>
2017-12-18 23:31:58 +0530

    day1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { AboutPage } from './../pages/about/about';
+import { BooksPage } from './../pages/books/books';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -7,10 +10,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    BooksPage,
+    AboutPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    BooksPage,
+    AboutPage
   ],
   providers: [
     StatusBar,
